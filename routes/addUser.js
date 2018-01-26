091bd10d29f9e78d6183094d78d8f614e814f74b
Kotaro Seto <setok@oregonstate.edu>
2018-01-26 12:14:06 +0900

    Add first and last name fields. Add form validation. Add password rule description. Add undo button for signature. Remove previously generated signature pngs.

--- routes/addUser.js
+++ routes/addUser.js
@@ -8,11 +8,13 @@ var bcrypt = require('bcrypt');
 router.post('/', function(req, res, next) {
 
 	// Image processing
-	var base64 = req.body.base64.substr(22); // Omitting prefix
 	var imgPath = 'public/signatures/';
 	var imgName = req.body.email+'_sigature';
 	var imgFullPath = '/' + imgPath + imgName + '.png';
 	base64Img.imgSync(req.body.base64, imgPath, imgName);
+
+	// Defining user type
+	var type = 'user';
 
 	// Password processing (async)
 	const saltRounds = 10;
@@ -27,19 +29,19 @@ router.post('/', function(req, res, next) {
 				console.log("ERROR HASHING: " + err);
 				res.send("ERROR HASHING: " + err);
 			}
-			console.log("email: " + req.body.email + " pass: " + hash + " imgPath: " + imgFullPath);
+			console.log('Sign up for \"' +  req.body.fname + " " + req.body.lname + '\" was successfull. Signature saved in: ' + imgFullPath); 
 			/* THIS IS FOR WHEN THE DB IS READY
-			mysql.pool.query("INSERT INTO users (`email`, `hash`, `imgPath`) VALUES (?,?,?",
-			[req.body.email, hash, imgFullPath],
+			mysql.pool.query("INSERT INTO users (`fname`, `lname`, `email`, `hash`, `imgPath`, `type`) VALUES (?,?,?,?,?,?",
+			[req.body.fname, req.body.lname, req.body.email, hash, imgFullPath, type],
 			function(err, res) {
 				if (err) {
 					console.log('SERVER ERROR: ' + err);
 					res.send('SERVER ERROR: ' + err);
 				}
-				res.send('Sign up for \"' +  req.body.email + '\" is successfull. Signature saved in: ' + imgFullPath);
+				res.send('Sign up for \"' +  req.body.email + '\" was successfull. Signature saved in: ' + imgFullPath);
 			});
 			*/
-			res.send('Sign up for \"' +  req.body.email + '\" is successfull. Signature saved in: ' + imgFullPath); // REMOVE THIS AFTER DB CONNECTION
+			res.send('Sign up for \"' +  req.body.fname + " " + req.body.lname + '\" was successfull. Signature saved in: ' + imgFullPath); // REMOVE THIS AFTER DB CONNECTION
 		});
 	});
 });
