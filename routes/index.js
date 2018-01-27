c79d447f10dc036e341012f8818e1a3a47c29c8b
Rachelle Robinett <rrobinett@webmd.net>
2018-01-27 15:28:31 -0800

    removing unneeded files

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', {
+		title: 'Some Page',
+		hideNav: 'hidden',
+		customHeader: '<link href="stylesheets/signin.css" rel="stylesheet" />'
+	}
+	);
 });
+
+function removeNavigation() {
+	document.getElementById("meunbutton").style.display = 'none';
+	document.getElementById("navgroup").style.display = 'none';
+} 
 
 module.exports = router;
