7a94dc7230d6b40e1622d17f0141401dc07c05f5
Rachelle Robinett <rrobinett@webmd.net>
2018-02-21 23:18:40 -0800

    full-ish featured awards dashboard

--- public/scripts/businessIntelligence.js
+++ public/scripts/businessIntelligence.js
@@ -1,6 +1,6 @@
 ﻿// Load the Visualization API and the corechart package.
 //google.charts.load('current', { packages: ['calendar'] });
-google.charts.load('current', { packages: ['corechart', 'table', 'bar', 'charteditor'] });
+google.charts.load('current', { packages: ['corechart', 'table', 'bar', 'charteditor', 'controls'] });
 
 // Set a callback to run when the Google Visualization API is loaded.
 google.charts.setOnLoadCallback(drawChart);
@@ -16,9 +16,10 @@ function drawChart() {
 
 	drawUserTypeChart();
 	drawUsersTable();
-	drawAwardsTable();
-	drawAwardTypeChart();
-	drawAwardsDomainChart();
+	//drawAwardsTable();
+	//drawAwardTypeChart();
+	//drawAwardsDomainChart();
+	drawAwardVisuals();
 	drawAwardsByUserChart();
 	drawAwardsCalendarTable();
 
@@ -171,6 +172,91 @@ async function drawAwardTypeChart() {
 
 }
 
+async function drawAwardVisuals() {
+	var apiData;
+
+	try {
+		const response = await axios.get('/api/reports/awards/table');
+		apiData = response.data;
+		//console.log(response.data);
+	} catch (error) {
+		console.log(error);
+		return;
+	}
+
+
+	var data = new google.visualization.arrayToDataTable(apiData);
+
+	var formatter_short = new google.visualization.DateFormat({ formatType: 'short' });
+	formatter_short.format(data, 4);
+
+
+	//create a dashboard
+	var dashboard = new google.visualization.Dashboard(
+		document.getElementById('awards_dashboard'));
+
+	//create a range slider
+	var dateRangeSlider = new google.visualization.ControlWrapper({
+		'controlType': 'DateRangeFilter',
+		'containerId': 'filter_awards_by_date',
+		'options': {
+			'filterColumnLabel': 'Granted',
+			//formatType: 'short'
+		},
+		//'ui': { 'label': 'Award Date' },
+		//'state': { lowThumbAtMinimum: true, highThumbAtMaximum: true} //
+		//state: { 'lowValue': '1970-01-01', 'highValue': '2070-1-1' }
+		//setting state and control title are not working
+	});
+
+	//create a range slider
+	var awardCategoryPicker = new google.visualization.ControlWrapper({
+		'controlType': 'CategoryFilter',
+		'containerId': 'filter_awards_by_type',
+		'options': {
+			'filterColumnLabel': 'Award Type',
+			//formatType: 'short'
+		},
+		//'ui': { 'label': 'Award Date' },
+		//'state': { lowThumbAtMinimum: true, highThumbAtMaximum: true} //
+		//state: { 'lowValue': '1970-01-01', 'highValue': '2070-1-1' }
+		//setting state and control title are not working
+	});
+
+	var tableWrapper = new google.visualization.ChartWrapper({
+		chartType: 'Table',
+		//	dataTable: result,
+		options: { showRowNumber: false, width: '100%', height: '100%' },
+		containerId: 'chart_awards_table'
+	});
+
+	var options = {
+		width: 1000,
+		height: 500,
+		isStacked: true
+	};
+
+
+	google.visualization.events.addListener(tableWrapper, 'ready', function () {
+		var chart = new google.visualization.ColumnChart(document.getElementById("chart_award_domain"));
+		var aggregatedData = google.visualization.data.group(
+			tableWrapper.getDataTable(),
+			[5],
+			[
+				{ 'column': 2, 'label': 'Month', 'pattern': 'month', 'aggregation': countMonthMatch, 'type': 'number' },
+				{ 'column': 2, 'label': 'Week', 'pattern': 'week', 'aggregation': countWeekMatch, 'type': 'number' }
+			]
+		);
+		chart.draw(aggregatedData, options);
+	});
+
+
+	dashboard.bind([dateRangeSlider, awardCategoryPicker], tableWrapper);
+	dashboard.draw(data);
+
+}
+
+
 async function drawAwardsTable() {
 
 	var apiData;
@@ -245,4 +331,23 @@ async function drawUserTypeChart() {
 	// Instantiate and draw our chart, passing in some options.
 	var chart = new google.visualization.PieChart(document.getElementById('chart_user_type'));
 	chart.draw(data, options);
+}
+
+
+function countWordMatch(values, word) {
+	var countWords = 0;
+	values.forEach(function (element) {
+		if (element.toLowerCase() === word.toLowerCase()) {
+			countWords += 1;
+		}
+	});
+	return countWords;
+}
+
+function countMonthMatch(values) {
+	return countWordMatch(values, 'month');
+}
+
+function countWeekMatch(values) {
+	return countWordMatch(values, 'week');
 }
