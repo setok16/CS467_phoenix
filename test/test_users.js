0efcbafce87c7b5ec9fa4925bf137806d00b31d7
Kotaro Seto <setok@oregonstate.edu>
2018-02-23 12:27:06 +0900

    Add tests for /users.

--- test/test_users.js
+++ test/test_users.js
@@ -17,9 +17,33 @@ var newName = {
   input_fname: splitName[0],
   input_lname: splitName[1]
 };
+var newPwd = 'Passw0rd';
+var newEmail= 'testEmail@test.com';
 
 
 describe('/users', function() {
+  
+  describe('/users', function() {
+
+    it ('should return status code of 200 if user page renders', function(done) {
+      var agent = chai.request.agent(app);
+      agent // Retaining session cookie
+      .post('/login')
+      .send({
+        email: process.env.EXISTING_BASIC_EMAIL,
+        password: process.env.EXISTING_BASIC_PASS })
+      .then(function(res) {
+        agent
+        .get('/users')
+        .send()
+        .then(function (res) {
+          res.should.have.status(200);
+          done();
+        });
+      });
+    });
+
+  });
   
   describe('/users/editName', function() {
 
@@ -79,5 +103,230 @@ describe('/users', function() {
     });
   });
 
+  describe('/users/changePwd', function() {
+
+    it ('should return 400 if passwords do not match', function(done) {
+      var passwords = {
+        input_pwd: 'P@ssw0rd',
+        input_pwd_verify: 'P@ssw0rb'
+      };
+      var agent = chai.request.agent(app);
+      agent // Retaining session cookie
+      .post('/login')
+      .send({
+        email: process.env.EXISTING_BASIC_EMAIL,
+        password: process.env.EXISTING_BASIC_PASS })
+      .then(function(res) {
+        agent
+        .patch('/users/changePwd')
+        .send(passwords)
+        .end(function (err, res) {
+          res.should.have.status(400);
+          done();
+        });
+      });
+    });
+
+    it ('should return 400 if password does not meet requirements', function(done) {
+      var passwords = {
+        input_pwd: 'password',
+        input_pwd_verify: 'password'
+      };
+      var agent = chai.request.agent(app);
+      agent // Retaining session cookie
+      .post('/login')
+      .send({
+        email: process.env.EXISTING_BASIC_EMAIL,
+        password: process.env.EXISTING_BASIC_PASS })
+      .then(function(res) {
+        agent
+        .patch('/users/changePwd')
+        .send(passwords)
+        .end(function (err, res) {
+          res.should.have.status(400);
+          done();
+        });
+      });
+    });
+
+    it ('should return 401 if passwords aren\'t specified', function(done) {
+      var passwords = {
+      };
+      var agent = chai.request.agent(app);
+      agent // Retaining session cookie
+      .post('/login')
+      .send({
+        email: process.env.EXISTING_BASIC_EMAIL,
+        password: process.env.EXISTING_BASIC_PASS })
+      .then(function(res) {
+        agent
+        .patch('/users/changePwd')
+        .send(passwords)
+        .end(function (err, res) {
+          res.should.have.status(400);
+          done();
+        });
+      });
+    });
+    
+    it ('should return status code of 200 if password is successfully changed', function(done) {
+      var passwords = {
+        input_pwd: newPwd,
+        input_pwd_verify: newPwd
+      };
+      var agent = chai.request.agent(app);
+      agent // Retaining session cookie
+      .post('/login')
+      .send({
+        email: process.env.EXISTING_BASIC_EMAIL,
+        password: process.env.EXISTING_BASIC_PASS })
+      .then(function(res) {
+        agent
+        .patch('/users/changePwd')
+        .send(passwords)
+        .end(function (err, res) {
+          res.should.have.status(200);
+          done();
+        });
+      });
+    });
+
+    it ('should allow login with new password after password change.', function(done) {
+      var agent = chai.request.agent(app);
+      agent // Retaining session cookie
+      .post('/login')
+      .send({
+        email: process.env.EXISTING_BASIC_EMAIL,
+        password: newPwd })
+      .then(function(res) {
+        res.should.have.status(200);
+        done();
+      });
+    });
+
+    it ('should change the password back to the original', function(done) {
+      var passwords = {
+        input_pwd: process.env.EXISTING_BASIC_PASS,
+        input_pwd_verify: process.env.EXISTING_BASIC_PASS
+      };
+      var agent = chai.request.agent(app);
+      agent // Retaining session cookie
+      .post('/login')
+      .send({
+        email: process.env.EXISTING_BASIC_EMAIL,
+        password: newPwd })
+      .then(function(res) {
+        agent
+        .patch('/users/changePwd')
+        .send(passwords)
+        .end(function (err, res) {
+          res.should.have.status(200);
+          done();
+        });
+      });
+    });
+
+  });
+
+  describe('/users/changeEmail', function() {
+
+    it ('should return 409 if email already exists', function(done) {
+      var email = {
+        input_email: process.env.EXISTING_BASIC_EMAIL
+      };
+      var agent = chai.request.agent(app);
+      agent // Retaining session cookie
+      .post('/login')
+      .send({
+        email: process.env.EXISTING_BASIC_EMAIL,
+        password: process.env.EXISTING_BASIC_PASS })
+      .then(function(res) {
+        agent
+        .patch('/users/changeEmail')
+        .send(email)
+        .end(function (err, res) {
+          res.should.have.status(409);
+          done();
+        });
+      });
+    });
+
+    it ('should return 400 if email has incorrect format' , function(done) {
+      var email = {
+        input_email: '12345'
+      };
+      var agent = chai.request.agent(app);
+      agent // Retaining session cookie
+      .post('/login')
+      .send({
+        email: process.env.EXISTING_BASIC_EMAIL,
+        password: process.env.EXISTING_BASIC_PASS })
+      .then(function(res) {
+        agent
+        .patch('/users/changeEmail')
+        .send(email)
+        .end(function (err, res) {
+          res.should.have.status(400);
+          done();
+        });
+      });
+    });
+
+    it ('should return 200 if email is successfully changed' , function(done) {
+      var email = {
+        input_email: newEmail
+      };
+      var agent = chai.request.agent(app);
+      agent // Retaining session cookie
+      .post('/login')
+      .send({
+        email: process.env.EXISTING_BASIC_EMAIL,
+        password: process.env.EXISTING_BASIC_PASS })
+      .then(function(res) {
+        agent
+        .patch('/users/changeEmail')
+        .send(email)
+        .end(function (err, res) {
+          res.should.have.status(200);
+          done();
+        });
+      });
+    });
+
+    it ('should allow login with new email after email change.', function(done) {
+      var agent = chai.request.agent(app);
+      agent // Retaining session cookie
+      .post('/login')
+      .send({
+        email: newEmail,
+        password: process.env.EXISTING_BASIC_PASS})
+      .then(function(res) {
+        res.should.have.status(200);
+        done();
+      });
+    });
+
+    it ('should change the email back to the original', function(done) {
+      var email = {
+        input_email: process.env.EXISTING_BASIC_EMAIL
+      };
+      var agent = chai.request.agent(app);
+      agent // Retaining session cookie
+      .post('/login')
+      .send({
+        email: newEmail,
+        password: process.env.EXISTING_BASIC_PASS })
+      .then(function(res) {
+        agent
+        .patch('/users/changeEmail')
+        .send(email)
+        .end(function (err, res) {
+          res.should.have.status(200);
+          done();
+        });
+      });
+    });
+
+  });
 
 });
